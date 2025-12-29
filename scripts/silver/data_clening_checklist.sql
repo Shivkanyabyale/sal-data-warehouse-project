e811e596a78dc2c3dede54aaed7ea034eeec695e
Shivkanya Sadanand Byale <123321449+Shivkanyabyale@users.noreply.github.com>
2025-12-30 00:25:24 +0530

    Update data_clening_checklist.sql

--- scripts/silver/data_clening_checklist.sql
+++ scripts/silver/data_clening_checklist.sql
@@ -58,3 +58,44 @@ FROM bronze.crm_prd_info
 SELECT * 
 FROM bronze.crm_prd_info
 where prd_end_dt < prd_start_dt
+
+-- Check for invalide dates 
+SELECT
+NULLIF(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR LEN(sls_order_dt) != 8
+
+--if dates are within the boundries 
+SELECT
+NULLIF(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+--Invalide date orders 
+SELECT 
+* 
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+--Check Data Consistancy between sales ,Quantity & Price 
+-- Sales= Quantity* Price
+--Values must not be NULL, Zero or Negative 
+
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details 
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR  sls_quantity <=0 OR sls_price <=0
+ORDER BY sls_sales , sls_quantity, sls_price
+
+--Identify out of range dates 
+SELECT DISTINCT
+bdate
+FROM bronze.erp_cust_az12
+WHERE bdate <'1924-01-01' OR bdate > GETDATE()
+
+
